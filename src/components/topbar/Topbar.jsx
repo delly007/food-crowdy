ede174cc0047679e899340fc1a8d3b374e0a2ef2
Ayodele Ogun <dellysmaide@gmail.com>
2021-07-01 10:38:12 +0100

    master

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React from "react";
+import { Language, NotificationsNone, SettingsOutlined } from "@material-ui/icons";
+import { Avatar, makeStyles } from "@material-ui/core";
+
+import './topbar.css';
+
+const useStyles = makeStyles((theme) => ({
+    small: {
+        width: theme.spacing(4),
+        height: theme.spacing(4),
+    },
+  }));
 
 export default function Topbar() {
-    return ( <
-        div className = "topbar" >
+    const classes = useStyles();
 
-        <
-        /div>
-    )
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topbarLeft">
+                    <span className="logo">FoodCrowdy</span>
+                </div>
+                <div className="topbarRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topbarIconBadge">17</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topbarIconBadge">17</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <SettingsOutlined />
+                    </div>
+                    <Avatar className={classes.small}>H</Avatar>
+                </div>
+            </div>
+        </div>
+    );
 }
